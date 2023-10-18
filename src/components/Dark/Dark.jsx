13e7099e511ddd80063f87dda122717b316fdccf
dilerr <jdilernia99@gmail.com>
2023-10-18 13:59:04 -0300

    home dark mode

--- src/components/Dark/Dark.jsx
+++ src/components/Dark/Dark.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import './Dark.css'
 import { BsMoonStars } from 'react-icons/bs'
 
@@ -10,7 +11,38 @@ import { BsGithub } from 'react-icons/bs'
 import { motion } from 'framer-motion'
 
 
+
+
 const Dark = () => {
+
+    const [dark, setDark] = useState(false)
+
+// cambiar la clase del dark-contenedor para el dark mode
+
+    const handleDark = () => {
+        if (dark === false) {
+            setDark(true)
+            document.querySelector('.dark-contenedor').classList.add('dark-contenedor-dark')
+            document.querySelector('.home-contenedor-social').classList.add('home-contenedor-social-dark')
+            document.querySelector('.dark-contenedor-icono').classList.add('dark-contenedor-icono-dark')
+            document.querySelector('.home-contenedor').classList.add('home-contenedor-dark')
+            document.querySelector('.home-contenedor-texto').classList.add('home-contenedor-texto-dark')
+            document.querySelector('.home-contenedor-iconos').classList.add('home-contenedor-iconos-dark')
+
+
+
+        } else {
+            setDark(false)
+            document.querySelector('.dark-contenedor').classList.remove('dark-contenedor-dark')
+            document.querySelector('.home-contenedor-social').classList.remove('home-contenedor-social-dark')
+            document.querySelector('.dark-contenedor-icono').classList.remove('dark-contenedor-icono-dark')
+            document.querySelector('.home-contenedor').classList.remove('home-contenedor-dark')
+            document.querySelector('.home-contenedor-texto').classList.remove('home-contenedor-texto-dark')
+            document.querySelector('.home-contenedor-iconos').classList.remove('home-contenedor-iconos-dark')
+
+            
+        }
+    }
 
     return (
         <div className='dark-contenedor'>
@@ -22,8 +54,9 @@ const Dark = () => {
 
             </motion.div>
             <motion.div initial={{ y: -50 }} animate={{ y: 0 }} transition={{ duration: 0.5 }} className='dark-contenedor-icono'>
-                <BsMoonStars />
+                <BsMoonStars onClick={handleDark} />
             </motion.div>
+
 
         </div>
 
